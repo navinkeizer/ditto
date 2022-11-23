eeee13aa8b8e243ae9f912e3aee9584850de5542
navinkeizer <46092420+navinkeizer@users.noreply.github.com>
2022-11-23 20:51:57 +0000

    Update performance_comparison.py

--- performance_comparison.py
+++ performance_comparison.py
@@ -1,5 +1,9 @@
 
+# todo: 1. implement the jaccard and accuracy comparisons from earlier runs on larger
+#  dataset containing 1) ipfs crawled data and 2) wikipedia data
 
+# todo: 2. implement modified kademlia with minhash in id space
+import operator
 import wikidata
 import numpy as np
 import progressbar
@@ -48,18 +52,22 @@ def save_ipfs_cids():
     file.close()
 
 
-def generate_sig(t, wd):
+def generate_sig(t, wd,  ks, num_perm):
     try:
+        t1 = time.time()
         txt_file = wd.get_content_txt(t)
-        shingles = kshingle.shingleset_k(txt_file, k=4)
-        m = MinHash(num_perm=64)
+        shingles = kshingle.shingleset_k(txt_file, k=ks)
+        m = MinHash(num_perm=num_perm)
         for d in shingles:
             m.update(d.encode('utf8'))
+        t2 = time.time()
+        delay = t2-t1
     except:
         print("Issue getting", t, "...")
         m=''
         shingles = ''
-    return m, shingles
+        delay = ''
+    return m, shingles, delay
 
 
 def get_change(current, previous):
@@ -152,33 +160,36 @@ def get_ipfs_signatures(number):
                 continue
 
 
-def compare_accuracy(percentage,w):
+def compare_accuracy(number,w, kshingle, num_perm):
     wd = wikidata.wikidata("wiki_nl_all.zim")
     titles = wd.get_titles_large()
-    # randomly take subset of x% and compare accuracy
     np.random.shuffle(titles)
-    subset = titles[:int(len(titles) * (percentage/100))]
+    subset = titles[:number]
     np.random.shuffle(titles)
-    subset2 = titles[:int(len(titles) * (percentage/100))]
+    subset2 = titles[:number]
     av_dif=0.
     n=0
 
     oo = 0
-    with progressbar.ProgressBar(max_value=len(subset), widgets=widgets) as bar:
+    with progressbar.ProgressBar(max_value=int(number*number), widgets=widgets) as bar:
         for s in subset:
-            bar.update(00)
-            oo += 1
             for a in subset2:
+                bar.update(oo)
+                oo += 1
                 if s != a:
-                    m1, s1 = generate_sig(s, wd)
-                    m2, s2 = generate_sig(a, wd)
+                    m1, s1, d1 = generate_sig(s, wd, kshingle, num_perm)
+                    m2, s2, d2 = generate_sig(a, wd, kshingle, num_perm)
                     if m1 != '' and m2 != '':
                         estimate = m1.jaccard(m2)
+                        # xor = (m1.digest()) ^ (m2.digest())
                         real = float(len(s1.intersection(s2)))/float(len(s1.union(s2)))
                         dif = get_change(real,estimate)
+                        # difxor = get_change(real,xor)
                         av_dif = av_dif + dif
+                        delay = (d1+d2)/2
                         n=n+1
-                        row = [percentage, dif]
+                        # row = [number, kshingle, num_perm, dif, delay, difxor]
+                        row = [number, kshingle, num_perm, dif, delay]
                         w.writerow(row)
 
     return av_dif/n
@@ -234,43 +245,124 @@ def compare_metrics(signatures, w1 ,w2, dataset):
 
 def main():
 
-    content_number = 100000
-    print("Getting wikipedia signatures...")
+    # content_number = 100000
+    # print("Getting wikipedia signatures...")
+    #
+    # f0 = open('delay_signatures.csv', 'a')
+    # writer0 = csv.writer(f0)
+    # s, delay = get_wiki_signatures(content_number, writer0)
+    # print("Average delay signatures", delay, "seconds")
+    # f0.close()
+    #
+    # print()
+    # print("Comparing signature distance metrics wikidata...")
+    #
+    # f1 = open('compare_metrics_all.csv', 'a')
+    # writer1 = csv.writer(f1)
+    # f2 = open('compare_metrics_global.csv', 'a')
+    # writer2 = csv.writer(f2)
+    # compare_metrics(s, writer1, writer2, "wikidata")
+    # f1.close()
+    # f2.close()
 
-    f0 = open('delay_signatures.csv', 'a')
-    writer0 = csv.writer(f0)
-    s, delay = get_wiki_signatures(content_number, writer0)
-    print("Average delay signatures", delay, "seconds")
-    f0.close()
 
-    print()
-    print("Comparing signature distance metrics wikidata...")
-
-    f1 = open('compare_metrics_all.csv', 'a')
-    writer1 = csv.writer(f1)
-    f2 = open('compare_metrics_global.csv', 'a')
-    writer2 = csv.writer(f2)
-    compare_metrics(s, writer1, writer2, "wikidata")
-    f1.close()
-    f2.close()
-
-
-    # todo: insert ipfs (move above comparing signature so files are still open)
     # write to the same files as wiki
     # print()
     # print("Getting ipfs signatures...")
     # print()
     # print("Comparing signature distance metrics ipfs...")
 
+    # todo run with nump = 4, 8, 256
 
-    print()
-    print("Checking jaccard accuracy vs raw data...")
-    f3 = open('accuracy_comparison.csv', 'a')
-    writer3 = csv.writer(f3)
+    nmr = 50
+    
+    k = 6
+    nump = [4,8,16,32,64,128,256]
+    for np in nump:
+        print()
+        print("Checking jaccard accuracy vs raw data...")
+        print(" Kshingle", str(k), "\n", "num_perm", str(np), "\n", "number of runs", str(nmr))
+        f3 = open('accuracy_comparison3.csv', 'a')
+        writer3 = csv.writer(f3)
+        av_dif = compare_accuracy(nmr, writer3, k, np)
 
-    av_dif = compare_accuracy(1, writer3)
-    print("Average difference signature vs raw (for wikidata): ",av_dif)
-    f3.close()
+        print("Average difference signature vs raw (for wikidata): ", av_dif)
+        f3.close()
+        
+        
+    np = 128
+    kss =[2, 4, 6, 8, 10, 12]
+    for k in kss:
+        print()
+        print("Checking jaccard accuracy vs raw data...")
+        print(" Kshingle", str(k), "\n", "num_perm", str(np), "\n", "number of runs", str(nmr))
+        f3 = open('accuracy_comparison3.csv', 'a')
+        writer3 = csv.writer(f3)
+        av_dif = compare_accuracy(nmr, writer3, k, np)
+
+        print("Average difference signature vs raw (for wikidata): ", av_dif)
+        f3.close()
+    
+        
+
+
+
+    # k = 4
+    # np = 16
+    #
+    # print()
+    # print("Checking jaccard accuracy vs raw data...")
+    # print(" Kshingle", str(k), "\n", "num_perm", str(np), "\n", "number of runs", str(nmr))
+    # f3 = open('accuracy_comparison2.csv', 'a')
+    # writer3 = csv.writer(f3)
+    # av_dif = compare_accuracy(nmr, writer3, k, np)
+    #
+    # print("Average difference signature vs raw (for wikidata): ", av_dif)
+    # f3.close()
+
+
+    # 
+    # k=4
+    # np = 4
+    # 
+    # print()
+    # print("Checking jaccard accuracy vs raw data...")
+    # print(" Kshingle", str(k), "\n", "num_perm", str(np),"\n","number of runs", str(nmr))
+    # f3 = open('accuracy_comparison2.csv', 'a')
+    # writer3 = csv.writer(f3)
+    # av_dif = compare_accuracy(nmr, writer3, k, np)
+    # 
+    # print("Average difference signature vs raw (for wikidata): ",av_dif)
+    # f3.close()
+
+
+    # k=4
+    # np = 8
+    #
+    # print()
+    # print("Checking jaccard accuracy vs raw data...")
+    # print(" Kshingle", str(k), "\n", "num_perm", str(np),"\n","number of runs", str(nmr))
+    # f3 = open('accuracy_comparison2.csv', 'a')
+    # writer3 = csv.writer(f3)
+    # av_dif = compare_accuracy(nmr, writer3, k, np)
+    #
+    # print("Average difference signature vs raw (for wikidata): ",av_dif)
+    # f3.close()
+    #
+    # k = 4
+    # np = 256
+    #
+    # print()
+    # print("Checking jaccard accuracy vs raw data...")
+    # print(" Kshingle", str(k), "\n", "num_perm", str(np), "\n", "number of runs", str(nmr))
+    # f3 = open('accuracy_comparison2.csv', 'a')
+    # writer3 = csv.writer(f3)
+    # av_dif = compare_accuracy(nmr, writer3, k, np)
+    #
+    # print("Average difference signature vs raw (for wikidata): ", av_dif)
+    # f3.close()
+
+
 
 
 if __name__ == '__main__':
